7ffe2929d66167ee5f2f0eeb73ead29bc8d4d04a
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-06-07 22:23:25 +0330

    complete without chatgbt and train

--- script.js
+++ script.js
@@ -1,86 +1,367 @@
 const response = [
   {
-    genres: "Action",
-    stepInfo: [
+    genres: "Sci-Fi",
+    data: [
       {
-        imageSrc: "./images/movie-image-1.jpg",
-        questions: ["step 1 - question 1", "step 1 - question 2"],
+        src: "./images/movie-image-1.jpg",
+        cate: "Sci-fi",
+        title: "Interstellar",
+        titleContent: "Interstellar (2015)",
+        rate: "7.4",
+        info: "2h 2min | Crime, Drama, Thriller",
+        descriptionText: `In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.`,
+        rateContent: "Rating: 9.0/10 | Mood: Dark",
       },
       {
-        imageSrc: "./images/movie-image-2.jpg",
-        questions: ["step 2 - question 1", "step 2 - question 2"],
+        src: "./images/movie-image-2.jpg",
+        cate: "Sci-fi",
+        title: "Blade Runner",
+        titleContent: "Blade Runner (2015)",
+        rate: "8.2",
+        info: "2h 17min | Action, Sci-Fi, Thriller",
+        descriptionText: `A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.`,
+        rateContent: "Rating: 8.1/10 | Mood: Futuristic",
       },
       {
-        imageSrc: "./images/movie-image-3.jpg",
-        questions: ["step 3 - question 1", "step 3 - question 2"],
+        src: "./images/movie-image-3.jpg",
+        cate: "Sci-fi",
+        title: "The Matrix",
+        titleContent: "The Matrix (2015)",
+        rate: "8.7",
+        info: "2h 16min | Action, Sci-Fi",
+        descriptionText: `A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.`,
+        rateContent: "Rating: 8.7/10 | Mood: Mind-bending",
       },
       {
-        imageSrc: "./images/movie-image-4.jpg",
-        questions: ["step 4 - question 1", "step 4 - question 2"],
+        src: "./images/movie-image-4.jpg",
+        cate: "Sci-fi",
+        title: "Inception",
+        titleContent: "Inception (2010)",
+        rate: "8.8",
+        info: "2h 28min | Action, Adventure, Sci-Fi",
+        descriptionText: `A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.`,
+        rateContent: "Rating: 8.8/10 | Mood: Mind-bending",
       },
     ],
   },
   {
-    genres: "Sci-Fi",
-    stepInfo: [
+    genres: "Action",
+    data: [
       {
-        imageSrc: "./images/movie-image-5.jpg",
-        questions: ["step 1 - question 1", "step 1 - question 2"],
+        src: "./images/movie-image-5.jpg",
+        cate: "Action",
+        title: "Mission: Impossible - Fallout",
+        titleContent: "Mission: Impossible - Fallout (2018)",
+        rate: "7.8",
+        info: "2h 27min | Action, Adventure, Thriller",
+        descriptionText: `Ethan Hunt and his IMF team, along with some familiar allies, race against time after a mission gone wrong.`,
+        rateContent: "Rating: 7.8/10 | Mood: Intense",
       },
       {
-        imageSrc: "./images/movie-image-6.jpg",
-        questions: ["step 2 - question 1", "step 2 - question 2"],
+        src: "./images/movie-image-6.jpg",
+        cate: "Action",
+        title: "John Wick",
+        titleContent: "John Wick (2014)",
+        rate: "7.4",
+        info: "1h 41min | Action, Crime, Thriller",
+        descriptionText: `An ex-hitman comes out of retirement to track down the gangsters that took everything from him.`,
+        rateContent: "Rating: 7.4/10 | Mood: Stylish",
       },
       {
-        imageSrc: "./images/movie-image-7.jpg",
-        questions: ["step 3 - question 1", "step 3 - question 2"],
+        src: "./images/movie-image-7.jpg",
+        cate: "Action",
+        title: "Mad Max: Fury Road",
+        titleContent: "Mad Max: Fury Road (2015)",
+        rate: "8.1",
+        info: "2h | Action, Adventure, Sci-Fi",
+        descriptionText: `In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search of her homeland with the aid of a group of female prisoners, a psychotic worshiper, and a drifter named Max.`,
+        rateContent: "Rating: 8.1/10 | Mood: Intense",
       },
       {
-        imageSrc: "./images/movie-image-8.jpg",
-        questions: ["step 4 - question 1", "step 4 - question 2"],
+        src: "./images/movie-image-8.jpg",
+        cate: "Action",
+        title: "The Dark Knight",
+        titleContent: "The Dark Knight (2008)",
+        rate: "9.0",
+        info: "2h 32min | Action, Crime, Drama",
+        descriptionText: `When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.`,
+        rateContent: "Rating: 9.0/10 | Mood: Dark",
       },
     ],
   },
   {
     genres: "Adventure",
-    stepInfo: [
+    data: [
       {
-        imageSrc: "./images/movie-image-9.jpg",
-        questions: ["step 1 - question 1", "step 1 - question 2"],
+        src: "./images/movie-image-9.jpg",
+        cate: "Adventure",
+        title: "Indiana Jones and the Raiders of the Lost Ark",
+        titleContent: "Indiana Jones and the Raiders of the Lost Ark (1981)",
+        rate: "8.4",
+        info: "1h 55min | Action, Adventure",
+        descriptionText: `Archaeologist and adventurer Indiana Jones is hired by the U.S. government to find the Ark of the Covenant before the Nazis.`,
+        rateContent: "Rating: 8.4/10 | Mood: Exciting",
       },
       {
-        imageSrc: "./images/movie-image-10.jpg",
-        questions: ["step 2 - question 1", "step 2 - question 2"],
+        src: "./images/movie-image-10.jpg",
+        cate: "Adventure",
+        title: "The Lord of the Rings: The Fellowship of the Ring",
+        titleContent:
+          "The Lord of the Rings: The Fellowship of the Ring (2001)",
+        rate: "8.8",
+        info: "2h 58min | Adventure, Drama, Fantasy",
+        descriptionText: `A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.`,
+        rateContent: "Rating: 8.8/10 | Mood: Epic",
       },
       {
-        imageSrc: "./images/movie-image-11.jpg",
-        questions: ["step 3 - question 1", "step 3 - question 2"],
+        src: "./images/movie-image-11.jpg",
+        cate: "Adventure",
+        title: "Pirates of the Caribbean: The Curse of the Black Pearl",
+        titleContent:
+          "Pirates of the Caribbean: The Curse of the Black Pearl (2003)",
+        rate: "8.0",
+        info: "2h 23min | Action, Adventure, Fantasy",
+        descriptionText: `Blacksmith Will Turner teams up with eccentric pirate Captain Jack Sparrow to save his love, the governor's daughter, from Jack's former pirate allies who are now undead.`,
+        rateContent: "Rating: 8.0/10 | Mood: Swashbuckling",
       },
       {
-        imageSrc: "./images/movie-image-12.jpg",
-        questions: ["step 4 - question 1", "step 4 - question 2"],
+        src: "./images/movie-image-12.jpg",
+        cate: "Adventure",
+        title: "The Jungle Book",
+        titleContent: "The Jungle Book (2016)",
+        rate: "7.4",
+        info: "1h 46min | Adventure, Drama, Family",
+        descriptionText: `After a threat from the tiger Shere Khan, a man-cub named Mowgli embarks on a journey of self-discovery with the help of a panther and a bear.`,
+        rateContent: "Rating: 7.4/10 | Mood: Heartwarming",
       },
     ],
   },
   {
     genres: "Comedy",
-    stepInfo: [
+    data: [
       {
-        imageSrc: "./images/movie-image-13.jpg",
-        questions: ["step 1 - question 1", "step 1 - question 2"],
+        src: "./images/movie-image-13.jpg",
+        cate: "Comedy",
+        title: "Anchorman: The Legend of Ron Burgundy",
+        titleContent: "Anchorman: The Legend of Ron Burgundy (2004)",
+        rate: "7.2",
+        info: "1h 34min | Comedy",
+        descriptionText: `Ron Burgundy is San Diego's top-rated newsman in the male-dominated broadcasting of the 1970s, but that's all about to change for Ron and his cronies when an ambitious woman is hired as a new anchor.`,
+        rateContent: "Rating: 7.2/10 | Mood: Hilarious",
       },
       {
-        imageSrc: "./images/movie-image-1.jpg",
-        questions: ["step 2 - question 1", "step 2 - question 2"],
+        src: "./images/movie-image-1.jpg",
+        cate: "Comedy",
+        title: "Superbad",
+        titleContent: "Superbad (2007)",
+        rate: "7.6",
+        info: "1h 53min | Comedy",
+        descriptionText: `Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.`,
+        rateContent: "Rating: 7.6/10 | Mood: Outrageous",
       },
       {
-        imageSrc: "./images/movie-image-2.jpg",
-        questions: ["step 3 - question 1", "step 3 - question 2"],
+        src: "./images/movie-image-2.jpg",
+        cate: "Comedy",
+        title: "Bridesmaids",
+        titleContent: "Bridesmaids (2011)",
+        rate: "6.8",
+        info: "2h 5min | Comedy, Romance",
+        descriptionText: `Competition between the maid of honor and a bridesmaid, over who is the bride's best friend, threatens to upend the life of an out-of-work pastry chef.`,
+        rateContent: "Rating: 6.8/10 | Mood: Hilarious",
       },
       {
-        imageSrc: "./images/movie-image-3.jpg",
-        questions: ["step 4 - question 1", "step 4 - question 2"],
+        src: "./images/movie-image-3.jpg",
+        cate: "Comedy",
+        title: "The Hangover",
+        titleContent: "The Hangover (2009)",
+        rate: "7.7",
+        info: "1h 40min | Comedy",
+        descriptionText: `Three buddies wake up from a bachelor party in Las Vegas, with no memory of the previous night and the bachelor missing. They make their way around the city in order to find their friend before his wedding.`,
+        rateContent: "Rating: 7.7/10 | Mood: Wild",
       },
     ],
   },
 ];
+
+const movieGrid = document.querySelector(".movie-grid");
+
+const removeAllCards = () => {
+  movieGrid.replaceChildren();
+};
+
+const createMovieItem = ({
+  src,
+  cate,
+  title,
+  rate,
+  info,
+  descriptionText,
+  titleContent,
+  rateContent,
+}) => {
+  const movieCard = createMovieCard(
+    src,
+    cate,
+    title,
+    titleContent,
+    rate,
+    info,
+    descriptionText,
+    rateContent
+  );
+  movieGrid.appendChild(movieCard);
+};
+
+const createMovieCard = (
+  src,
+  cate,
+  title,
+  titleContent,
+  rate,
+  info,
+  descriptionText,
+  rateContent
+) => {
+  const imageContainer = createImageContainer(src, cate, title, rate);
+  const detailContainer = createMovieDetail(
+    titleContent,
+    info,
+    descriptionText,
+    rateContent
+  );
+  const movieCard = document.createElement("article");
+  movieCard.classList.add("movie-card");
+  movieCard.appendChild(imageContainer);
+  movieCard.appendChild(detailContainer);
+  return movieCard;
+};
+
+const createImageContainer = (src, cate, title, rate) => {
+  const image = createImage(src);
+  const TitleIn = createTitleIn(cate, title, rate);
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+  imageContainer.appendChild(image);
+  imageContainer.appendChild(TitleIn);
+  return imageContainer;
+};
+
+const createImage = (src) => {
+  const movieImage = document.createElement("img");
+  movieImage.classList.add("movie-image");
+  movieImage.setAttribute("src", src);
+  const a = document.createElement("a");
+  a.appendChild(movieImage);
+  const div = document.createElement("div");
+  div.appendChild(a);
+  return div;
+};
+
+const createTitleIn = (cateContent, titleContent, rate) => {
+  const a = document.createElement("a");
+  a.innerText = cateContent;
+  const span = document.createElement("span");
+  span.appendChild(a);
+  const cate = document.createElement("div");
+  cate.classList.add("cate");
+  cate.appendChild(span);
+
+  const title = document.createElement("a");
+  title.innerText = titleContent;
+  const heading = document.createElement("h6");
+  heading.classList.add("movie-item__title-in__heading");
+  heading.appendChild(title);
+
+  let paragraph = document.createElement("p");
+  paragraph.classList.add("rate");
+
+  let starIcon = document.createElement("i");
+  starIcon.classList.add("fa-fw", "fa-md", "fa-star", "fa");
+  paragraph.appendChild(starIcon);
+
+  let rateValue = document.createElement("span");
+  rateValue.textContent = rate;
+  paragraph.appendChild(rateValue);
+
+  let rateSlash = document.createTextNode("/");
+  paragraph.appendChild(rateSlash);
+
+  let rateMax = document.createTextNode("10");
+  paragraph.appendChild(rateMax);
+
+  const TitleIn = document.createElement("div");
+  TitleIn.classList.add("movie-item__title-in");
+  TitleIn.appendChild(cate);
+  TitleIn.appendChild(heading);
+  TitleIn.appendChild(paragraph);
+
+  return TitleIn;
+};
+
+function createMovieDetail(
+  titleContent,
+  infoContent,
+  descriptionText,
+  rateContent
+) {
+  let container = document.createElement("div");
+  container.classList.add("detail-container");
+
+  let heading = document.createElement("p");
+  heading.classList.add("movie-heading");
+  heading.textContent = titleContent;
+  container.appendChild(heading);
+
+  let hr = document.createElement("hr");
+  hr.classList.add("hr-text");
+  container.appendChild(hr);
+
+  let info = document.createElement("div");
+  info.classList.add("info");
+  let infoText = document.createElement("span");
+  infoText.textContent = infoContent;
+  info.appendChild(infoText);
+  container.appendChild(info);
+
+  let description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = descriptionText;
+  container.appendChild(description);
+
+  let rate = document.createElement("p");
+  rate.classList.add("rate");
+  rate.textContent = rateContent;
+  container.appendChild(rate);
+
+  let buttons = document.createElement("div");
+  buttons.classList.add("buttons");
+  let trailerButton = document.createElement("button");
+  trailerButton.setAttribute("type", "button");
+  trailerButton.classList.add("button");
+  trailerButton.textContent = "TRAILER";
+  buttons.appendChild(trailerButton);
+  let imdbButton = document.createElement("button");
+  imdbButton.setAttribute("type", "button");
+  imdbButton.classList.add("button");
+  imdbButton.textContent = "IMDB";
+  buttons.appendChild(imdbButton);
+  container.appendChild(buttons);
+
+  return container;
+}
+
+// createMovieItem({ ...response[0].data[0] });
+
+function submitForm(event) {
+  removeAllCards();
+  event.preventDefault();
+  var genres = document.querySelector("form").elements["genres"].value;
+
+  for (const apiData of response) {
+    if (apiData.genres.toLocaleLowerCase() === genres.toLocaleLowerCase()) {
+      for (const data of apiData.data) {
+        createMovieItem({ ...data });
+      }
+    }
+  }
+}
