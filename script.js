ff5448699cc6f65f259c38e2113b1c3637403249
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-06-07 19:27:05 +0330

    .

---
+++ script.js
@@ -0,0 +1,86 @@
+const response = [
+  {
+    genres: "Action",
+    stepInfo: [
+      {
+        imageSrc: "./images/movie-image-1.jpg",
+        questions: ["step 1 - question 1", "step 1 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-2.jpg",
+        questions: ["step 2 - question 1", "step 2 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-3.jpg",
+        questions: ["step 3 - question 1", "step 3 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-4.jpg",
+        questions: ["step 4 - question 1", "step 4 - question 2"],
+      },
+    ],
+  },
+  {
+    genres: "Sci-Fi",
+    stepInfo: [
+      {
+        imageSrc: "./images/movie-image-5.jpg",
+        questions: ["step 1 - question 1", "step 1 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-6.jpg",
+        questions: ["step 2 - question 1", "step 2 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-7.jpg",
+        questions: ["step 3 - question 1", "step 3 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-8.jpg",
+        questions: ["step 4 - question 1", "step 4 - question 2"],
+      },
+    ],
+  },
+  {
+    genres: "Adventure",
+    stepInfo: [
+      {
+        imageSrc: "./images/movie-image-9.jpg",
+        questions: ["step 1 - question 1", "step 1 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-10.jpg",
+        questions: ["step 2 - question 1", "step 2 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-11.jpg",
+        questions: ["step 3 - question 1", "step 3 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-12.jpg",
+        questions: ["step 4 - question 1", "step 4 - question 2"],
+      },
+    ],
+  },
+  {
+    genres: "Comedy",
+    stepInfo: [
+      {
+        imageSrc: "./images/movie-image-13.jpg",
+        questions: ["step 1 - question 1", "step 1 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-1.jpg",
+        questions: ["step 2 - question 1", "step 2 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-2.jpg",
+        questions: ["step 3 - question 1", "step 3 - question 2"],
+      },
+      {
+        imageSrc: "./images/movie-image-3.jpg",
+        questions: ["step 4 - question 1", "step 4 - question 2"],
+      },
+    ],
+  },
+];
